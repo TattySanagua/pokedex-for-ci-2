41a232e52f251783c95d64a0770ab9a92ca87059
tatiana_sanagua <tati_sanagua@hotmail.com>
2024-03-09 11:27:22 -0300

    Corrections for Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   video: false,
   e2e: {
-    testingType: "e2e",
-    integrationFolder: "cypress/e2e",
-    baseUrl: "http://localhost:5000"
+    testingType: 'e2e',
+    integrationFolder: 'cypress/e2e',
+    baseUrl: 'http://localhost:5000'
   },
 }
