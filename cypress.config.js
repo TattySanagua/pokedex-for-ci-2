76a6549e50c70e8918939a1f8610201aec174821
tatiana_sanagua <tati_sanagua@hotmail.com>
2024-03-09 11:39:52 -0300

    Changes for cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ module.exports = {
   video: false,
   e2e: {
     testingType: 'e2e',
-    integrationFolder: 'cypress/e2e',
+    specPattern: 'cypress/e2e/**/*.spec.js',
     baseUrl: 'http://localhost:5000'
   },
 }
